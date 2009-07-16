fdd29abb4008510461f22ec2aa26bea182a31921
Valery V. Vorotyntsev <valery.vv@gmail.com>
2009-07-16 13:59:50 +0300

    Lab.TLV: `tag' implemented using State monad

--- Lab/TLV.hs
+++ Lab/TLV.hs
@@ -4,8 +4,8 @@ module Lab.TLV ( Tag (..), tag ) where
 import BBTest.Util (takeExactly)
 
 import qualified Data.ByteString.Lazy.Char8 as C
+import Control.Monad.State
 import Data.Char (isDigit)
--- import Control.Monad.State
 
 type TagID = Char
 
@@ -15,15 +15,41 @@ data Tag = ComplU TagID C.ByteString -- ^ Complex tag with unparsed content
            deriving (Show, Eq)
 
 tag :: C.ByteString -> (Either String Tag, C.ByteString)
+tag = runState tag1
 
+tag1 :: State C.ByteString (Either String Tag)
+tag1 = do str <- get
+          case C.uncons str of
+            Nothing       -> err "EOF"
+            Just ('F', s) -> put s >> tag1
+            Just ('P', s) -> g Prim   2 s
+            Just ('C', s) -> g ComplU 3 s
+            _ -> err "unknown tag type"
+    where
+      err = return . Left
+      g mktag nbytes s =
+          case takeExactly nbytes s of
+            Nothing -> err "incomplete tag"
+            Just (idlen, s') ->
+                let (tid, len) = (C.head idlen, C.tail idlen)
+                in if C.all isDigit len
+                   then case takeExactly (read $ C.unpack len) s' of
+                          Nothing -> err "not enough content octets"
+                          Just (content, rest) -> do
+                                      put rest
+                                      return $ Right $ mktag tid content
+                   else err "invalid length encoding"
+
+{-
+tag :: C.ByteString -> (Either String Tag, C.ByteString)
 tag str = case C.uncons str of
-            Nothing        -> (Left "EOF", C.empty)
+            Nothing        -> (Left "EOF", str)
             Just ('F', s1) -> tag s1
             Just ('P', s1) -> readTag Prim   2 s1
             Just ('C', s1) -> readTag ComplU 3 s1
-            Just _ -> (Left "unknown tag type", str)
+            _ -> (Left "unknown tag type", str)
     where
-      readTag tagType nbytes s =
+      readTag mktag nbytes s =
           case takeExactly nbytes s of
             Nothing -> (Left "incomplete tag", str)
             Just (idlen, s') ->
@@ -32,5 +58,6 @@ tag str = case C.uncons str of
                    then case takeExactly (read $ C.unpack len) s' of
                           Nothing -> (Left "not enough content octets", str)
                           Just (content, rest)
-                              -> (Right $ tagType tid content, rest)
+                              -> (Right $ mktag tid content, rest)
                    else (Left "invalid length encoding", str)
+-}
