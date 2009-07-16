bc066f479625ab29895f324392d0d8f4b919f38b
Valery V. Vorotyntsev <valery.vv@gmail.com>
2009-07-16 17:20:40 +0300

    monad transformers step by step

--- Lab/TLV.hs
+++ Lab/TLV.hs
@@ -1,23 +1,109 @@
 -- | Type-length-value decoding operations.
-module Lab.TLV ( Tag (..), tag ) where
+module Lab.TLV where
+-- module Lab.TLV ( Tag (..), tag ) where
 
 import BBTest.Util (takeExactly)
 
-import qualified Data.ByteString.Lazy.Char8 as C
+import Control.Monad.Identity
+import Control.Monad.Error
 import Control.Monad.State
+
+import qualified Data.ByteString.Lazy.Char8 as C
 import Data.Char (isDigit)
+
+type BStr = C.ByteString
 
 type TagID = Char
 
-data Tag = ComplU TagID C.ByteString -- ^ Complex tag with unparsed content
---          | Compl TagID [Tag]         -- ^ Complex tag
-         | Prim TagID C.ByteString   -- ^ Primitive tag
+data Tag = Prim TagID BStr   -- ^ Primitive tag
+--          | Compl TagID [Tag] -- ^ Complex tag
+         | ComplU TagID BStr -- ^ Complex tag with unparsed content
            deriving (Show, Eq)
 
-tag :: C.ByteString -> (Either String Tag, C.ByteString)
-tag = runState tag1
+example = C.pack "PE3Howdy!"    -- XXX
+test    = runEval4 tag4 example -- XXX
+testErr = runEval4 tag4 C.empty -- XXX
+tag = runEval4 tag4 -- XXX
 
-tag1 :: State C.ByteString (Either String Tag)
+-- XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+
+type Eval4 a = ErrorT String (StateT BStr Identity) a
+runEval4 :: Eval4 a -> BStr -> (Either String a, BStr)
+runEval4 ev s = runIdentity (runStateT (runErrorT ev) s)
+
+tag4 :: Eval4 Tag
+tag4 = do s <- get
+          case C.uncons s of
+            Nothing -> throwError "EOF"
+            Just ('F', s') -> put s' >> tag4
+            Just ('P', s') -> g Prim   2 s'
+            Just ('C', s') -> g ComplU 3 s'
+            _ -> throwError "unknown tag type"
+    where
+      g mktag nbytes s = do
+        case takeExactly nbytes s of
+          Nothing -> throwError "incomplete tag"
+          Just (idlen, s') ->
+              let (tid, len) = (C.head idlen, C.tail idlen)
+              in if C.all isDigit len
+                 then case takeExactly (read $ C.unpack len) s' of
+                        Nothing -> throwError "not enough content octets"
+                        Just (content, rest) -> do
+                                    put rest
+                                    return (mktag tid content)
+                 else throwError "invalid length encoding"
+
+
+type Eval3 a = ErrorT String Identity a
+runEval3 :: Eval3 a -> Either String a
+runEval3 = runIdentity . runErrorT
+
+tag3 :: BStr -> Eval3 Tag
+tag3 str = case C.uncons str of
+             Nothing -> throwError "EOF"
+             Just ('F', s) -> tag3 s
+             Just ('P', s) -> g Prim 2 s
+             Just ('C', s) -> g ComplU 3 s
+             _ -> throwError "unknown tag type"
+    where
+      g mktag nbytes s =
+          case takeExactly nbytes s of
+            Nothing -> throwError "incomplete tag"
+            Just (idlen, s') ->
+                let (tid, len) = (C.head idlen, C.tail idlen)
+                in if C.all isDigit len
+                   then case takeExactly (read $ C.unpack len) s' of
+                          Nothing -> throwError "not enough content octets"
+                          Just (content, _) -> return (mktag tid content)
+                   else throwError "invalid length encoding"
+
+
+type Eval2 a = Identity a
+runEval2 :: Eval2 a -> a
+runEval2 = runIdentity
+
+tag2 :: BStr -> Eval2 Tag
+tag2 str = case C.uncons str of
+             Nothing -> error "EOF"
+             Just ('F', s) -> tag2 s
+             Just ('P', s) -> g Prim 2 s
+             Just ('C', s) -> g ComplU 3 s
+             _ -> error "unknown tag type"
+    where
+      g :: (TagID -> BStr -> Tag) -> Int -> BStr -> Identity Tag
+      g mktag nbytes s =
+          case takeExactly nbytes s of
+            Nothing -> error "incomplete tag"
+            Just (idlen, s') ->
+                let (tid, len) = (C.head idlen, C.tail idlen)
+                in if C.all isDigit len
+                   then case takeExactly (read $ C.unpack len) s' of
+                          Nothing -> error "not enough content octets"
+                          Just (content, _) -> return (mktag tid content)
+                   else error "invalid length encoding"
+
+
+tag1 :: State BStr (Either String Tag)
 tag1 = do str <- get
           case C.uncons str of
             Nothing       -> err "EOF"
@@ -40,11 +126,11 @@ tag1 = do str <- get
                                       return $ Right $ mktag tid content
                    else err "invalid length encoding"
 
-{-
-tag :: C.ByteString -> (Either String Tag, C.ByteString)
-tag str = case C.uncons str of
+
+tag0 :: BStr -> (Either String Tag, BStr)
+tag0 str = case C.uncons str of
             Nothing        -> (Left "EOF", str)
-            Just ('F', s1) -> tag s1
+            Just ('F', s1) -> tag0 s1
             Just ('P', s1) -> readTag Prim   2 s1
             Just ('C', s1) -> readTag ComplU 3 s1
             _ -> (Left "unknown tag type", str)
@@ -60,4 +146,3 @@ tag str = case C.uncons str of
                           Just (content, rest)
                               -> (Right $ mktag tid content, rest)
                    else (Left "invalid length encoding", str)
--}
